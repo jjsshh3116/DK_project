4573a2cd271261fd1374801a7b4cb7042d428533
정세현 <jeongsehyeon@MacBook-Pro.local>
2024-04-29 17:13:30 +0900

    error fix4

--- ta/network_TA.c
+++ ta/network_TA.c
@@ -80,7 +80,7 @@ void forward_network_TA()
 
     roundnum++;
 
-    layer_TA l = netta.layers[i];
+    layer_TA l = netta.layers[netta.index];
 
     if(l.delta){
         fill_cpu_TA(l.outputs * l.batch, 0, l.delta, 1);
@@ -91,7 +91,7 @@ void forward_network_TA()
     if(debug_summary_pass == 1){
         summary_array("forward_network / l.output", l.output, l.outputs*netta.batch);
     }
-    
+
     // int i;
     // for(i = 0; i < netta.n; ++i){
     //     netta.index = i;
