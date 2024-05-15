f9829ab2bceb30d7428fd6a24edc66d3628f1afe
정세현 <jeongsehyeon@MacBook-Pro.local>
2024-05-16 01:32:00 +0900

    conv result print test

--- host/src/convolutional_layer.c
+++ host/src/convolutional_layer.c
@@ -506,9 +506,9 @@ void forward_convolutional_layer(convolutional_layer l, network net)
                 im2col_cpu(im, l.c/l.groups, l.h, l.w, l.size, l.stride, l.pad, b);
             }
             gemm(0,0,m,n,k,1,a,k,b,n,1,c,n);
-            // for(int z = 0; z < l.outputs; z++){
-            //     printf("normal %d: %f\n", z, c[z]);
-            // }
+            for(int z = 0; z < l.outputs; z++){
+                printf("normal C output[%d]: %f\n", z, c[z]);
+            }
         }
     }
 
