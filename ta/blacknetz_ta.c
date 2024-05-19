d96888ea923229b30fad1603af4f61295f03194e
정세현 <jeongsehyeon@MacBook-Pro.local>
2024-05-19 15:26:23 +0900

    TA l.output memory address testing

--- ta/blacknetz_ta.c
+++ ta/blacknetz_ta.c
@@ -550,7 +550,7 @@ static TEE_Result black_forward_network_TA_params(uint32_t param_types,
 
     layer_TA l = netta.layers[net_index];
 
-    IMSG("black_forward_network_TA_params l.output memory address: %d\n", netta.layers[net_index].output);
+    IMSG("black_forward_network_TA_params netta.layers[net_index].output memory address: %d\n", netta.layers[net_index].output);
     IMSG("black_forward_network_TA_params c memory address: %d\n", c);
 
     // IMSG("black conv output");
