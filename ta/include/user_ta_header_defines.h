44a3d063dcb8618623fae33fb292282051b62053
정세현 <jeongsehyeon@MacBook-Pro.local>
2024-07-02 09:56:48 +0900

    TA_DATA_SIZE change

--- ta/include/user_ta_header_defines.h
+++ ta/include/user_ta_header_defines.h
@@ -17,7 +17,7 @@
 #define TA_STACK_SIZE			(1 * 1024 * 1024)
 
 /* Provisioned heap size for TEE_Malloc() and friends */
-#define TA_DATA_SIZE			(5 * 1024 * 1024)
+#define TA_DATA_SIZE			(8 * 1024 * 1024)
 
 /* Extra properties (give a version id and a string name) */
 #define TA_CURRENT_TA_EXT_PROPERTIES \
